1cd6e49a67e80b656ce831c8d144edfdb3b7f9fd
Dodge164 <dodge164@mail.ru>
2021-07-02 17:00:48 +0400

    Add Forms

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './app-header.css';
 
-const AppHeader = () => {
+const AppHeader = ({ allPosts, liked }) => {
   return (
     <div className="app-header d-flex">
       <h1>Irishka Che</h1>
-      <h2>5 records, liked 0</h2>
+      <h2>
+        {allPosts} records, liked {liked}
+      </h2>
     </div>
   );
 };
